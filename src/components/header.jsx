b9f0aa4e6d79a4c0cc9bed0eca19c869badba3a6
Jesus Sanchez <jsancher@ieee.org>
2020-09-01 13:07:10 -0500

    Responsive Header (Mobile and Tablet)

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from '../assets/img/Desat_logo.png'
 
 function header(){
     return(
@@ -7,10 +8,14 @@ function header(){
                 <div className="header-filter absolute">
                 </div>
             </div>
+            <img className="logo absolute" src={logo} alt="Desat Logo"/>
             <div className="header-content-container absolute">
                 <h1 className="mont green">Tu aliado en</h1>
                 <h1 className="mont dark-green"><b>Seguridad</b></h1>
+                <h5 className="mont dark-grey">¡Tus trabajadores en las mejores manos!</h5>
+                <button className="btn main-btn dark-green-bg green mont">Servicios</button>
             </div>
+            <button className="btn main-btn dark-green-bg green mont absolute">Servicios</button>
         </div>
     )
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import logo from '../assets/img/Desat_logo.png'
 function Nav(){
     return(
-        <div className="row nav-bar fixed">
-            <div className="col-sm-6 flex-center-self"><img className="logo" src={logo} alt="Desat Logo"/></div>
-            <div className="col-sm-6"> Hola 2</div>
+        <div className="row nav-bar fixed w-100">
+            <div className="col-sm-6"></div>
+            <div className="col-sm-6"></div>
         </div>  
         
     )
