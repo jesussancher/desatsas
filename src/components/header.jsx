0b7fe3c094c2533071f429309b5c3f2256410225
Jesus Sanchez <jsancher@ieee.org>
2020-09-08 16:52:26 -0500

    Nav bar added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../assets/img/Desat_logo.png'
+// import logo from '../assets/img/Desat_logo.png'
 
 function header(){
     return(
@@ -8,14 +8,14 @@ function header(){
                 <div className="header-filter absolute">
                 </div>
             </div>
-            <img className="logo absolute" src={logo} alt="Desat Logo"/>
-            <div className="header-content-container absolute">
-                <h1 className="mont green">Tu aliado en</h1>
-                <h1 className="mont dark-green"><b>Seguridad</b></h1>
-                <h5 className="mont dark-grey">¡Tus trabajadores en las mejores manos!</h5>
+            {/* <img className="logo absolute" src={logo} alt="Desat Logo"/> */}
+            <div className="header-content-container hcc-md absolute">
+                <h1 className="h1-md mont green">Tu aliado en</h1>
+                <h1 className="h1-md mont dark-green"><b>Seguridad</b></h1>
+                <h5 className="h5-md mont dark-grey">¡Tus trabajadores en las mejores manos!</h5>
                 <button className="btn main-btn dark-green-bg green mont">Servicios</button>
             </div>
-            <button className="btn main-btn dark-green-bg green mont absolute">Servicios</button>
+            <button className="btn main-btn main-btn-md dark-green-bg green mont absolute">Servicios</button>
         </div>
     )
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,8 +3,18 @@ import logo from '../assets/img/Desat_logo.png'
 function Nav(){
     return(
         <div className="row nav-bar fixed w-100">
-            <div className="col-sm-6"></div>
-            <div className="col-sm-6"></div>
+            <div className="col-lg-6 col-md-5">
+            <img className="logo absolute" src={logo} alt="Desat Logo"/>
+            </div>
+            <div className="col-lg-6 col-md-7">
+                <ul className="nav-bar mont">
+                    <li><a>Inicio</a></li>
+                    <li><a>Nosotros</a></li>
+                    <li><a>Servicios</a></li>
+                    <li><a>Contacto</a></li>
+                    <a className="cert-btn">Certificado</a>
+                </ul>
+            </div>
         </div>  
         
     )
